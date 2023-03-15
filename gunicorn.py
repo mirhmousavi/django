cb30c2a21cf0271a38875db60e54747866a2e5f8
Mir <mir@surfly.com>
2023-03-15 13:28:34 +0100

    wip

--- gunicorn.py
+++ gunicorn.py
@@ -6,5 +6,5 @@ capture_output = True
 
 bind = ['0.0.0.0:5000']
 
-workers = os.getenv('GUNICORN_WORKERS')
+workers = int(os.getenv('GUNICORN_WORKERS'))
 worker_class = os.getenv('GUNICORN_WORKER_CLASS')
