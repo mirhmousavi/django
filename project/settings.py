34e2c32f8cdd21d9c44961d3b3fbcb7a6c740d10
Mir <mir@surfly.com>
2023-03-15 16:46:28 +0100

    wip

--- project/settings.py
+++ project/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
 
