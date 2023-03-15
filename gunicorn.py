8e03daee488e2c3b7043b0752af2d135537290a6
Mir <mir@surfly.com>
2023-03-15 16:45:40 +0100

    wip

--- gunicorn.py
+++ gunicorn.py
@@ -5,6 +5,8 @@ accesslog = '-'
 capture_output = True
 
 bind = ['0.0.0.0:5000']
+max_requests = 10000
+max_requests_jitter = 1000
 
 workers = int(os.getenv('GUNICORN_WORKERS'))
 worker_class = os.getenv('GUNICORN_WORKER_CLASS')
